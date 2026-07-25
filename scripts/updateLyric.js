c1b2c11ec2676859fb2bfd7a8e7e661586505522
sho <shosomething47@gmail.com>
2026-07-25 09:38:11 +0200

    Update updateLyric.js

--- scripts/updateLyric.js
+++ scripts/updateLyric.js
@@ -1,1 +1,83 @@
+import {writeFile} from "fs/promises";
 
+const API_KEY = process.env.LASTFM_API_KEY;
+const USER = process.env.LASTFM_USERNAME;
+
+if (!API_KEY || !USER) {
+  throw new Error("Missing api key or username");
+}
+
+const lastfmUrl = 
+  `https://ws.audioscrobbler.com/2.0/` +
+  `?method=user.gettoptracks` +
+  `&user=${encodeURIComponent(USER)}` +
+  `&period=7day` +
+  `&limit=10` +
+  `&api_key=${API_KEY}` +
+  `&format=json`;
+
+const lastfmRes = await fetch(lastfmUrl);
+const lastfmData = await lastfmRes.json();
+
+const tracks = lastfmData.toptrack?.track;
+
+if (!tracks || tracks.length === 0) {
+  throw new Error("no tracks :(");
+}
+
+const track =
+  tracks[Math.floor(Math.random() * tracks.length)];
+
+const artist = track.artist.name;
+const song = track.name;
+const plays = Number(track.playcount);
+
+console.log('Selected: ${artist}\'s ${song}');
+
+let lyric = "lyrics unaviable 3:";
+
+const lyricsUrl =
+  `https://lrclib.net/api/get` +
+  `?artist_name=${encodeURIComponent(artist)}` +
+  `&track_name=${encodeURIComponent(song)}`;
+
+const lyricsRes = await fetch(lyricsURL);
+
+if (lyricsRes.ok) {
+  const lyricsData = await lyricsRes.json();
+
+  const rawLyrics =
+    lyricsData.plainLyrics ||
+    lyricsData.syncedLyrics ||
+    "";
+
+  const lines = rawLyrics
+    .split("\n")
+    .map(line => line.trim())
+    .filter(line => {
+      return (
+        line.length > 0 &&
+        !line.startsWith("[")
+      );
+    });
+
+  if (lines.length > 0) {
+    lyric =
+      lines[Math.floor(Math.random() * lines.length)];
+  }
+}
+
+const output = {
+  date: new Date().toISOString().slice(0,10),
+  artist,
+  song,
+  plays,
+  lyric
+};
+
+await writeFile(
+  "today.json",
+  JSON.stringify(output, null, 2)
+);
+
+console.log("today.json updated");
